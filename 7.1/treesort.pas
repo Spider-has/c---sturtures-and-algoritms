8ebb8339082d9109b9a6afab6a642a37931dbe65
Spider-has <romanh998@gmail.com>
2023-10-25 01:05:06 +0300

    format fix

--- 7.1/treesort.pas
+++ 7.1/treesort.pas
@@ -8,88 +8,87 @@ INTERFACE
   PROCEDURE TreePrintStraight();
   PROCEDURE TreePrintReverse();
 IMPLEMENTATION
-TYPE
-  NodeTree = ^Tree;
-  Tree = RECORD
-           Key: DataType;
-           Left: NodeTree;
-           Right: NodeTree
-          END;
-  VAR
-    Root: NodeTree;
+  TYPE
+    NodeTree = ^Tree;
+    Tree = RECORD
+             Key: DataType;
+             Left: NodeTree;
+             Right: NodeTree
+            END;
+    VAR
+      Root: NodeTree;
     
     
-PROCEDURE Insert(VAR PTR: NodeTree; Data: DataType);
-BEGIN
-  IF PTR = NIL
-  THEN
-    BEGIN
-      New(PTR);
-      PTR^.Key := Data;
-      PTR^.Left := NIL;
-      PTR^.Right := NIL
-    END
-  ELSE
-    IF PTR^.Key > Data
+  PROCEDURE Insert(VAR PTR: NodeTree; Data: DataType);
+  BEGIN
+    IF PTR = NIL
     THEN
-      Insert(PTR^.Left, Data)
+      BEGIN
+        New(PTR);
+        PTR^.Key := Data;
+        PTR^.Left := NIL;
+        PTR^.Right := NIL
+      END
     ELSE
-      Insert(PTR^.Right, Data)
-END;
+      IF PTR^.Key > Data
+      THEN
+        Insert(PTR^.Left, Data)
+      ELSE
+        Insert(PTR^.Right, Data)
+  END;
 
-PROCEDURE TreeInsert(Data: DataType);
-BEGIN
-  Insert(Root, Data);
-  //WRITE(Data)
-END;
+  PROCEDURE TreeInsert(Data: DataType);
+  BEGIN
+    Insert(Root, Data)
+  END;
 
-PROCEDURE PrintSimmetry(PTR: NodeTree);
-BEGIN
-  IF PTR <> NIL
-  THEN
-    BEGIN
-      PrintSimmetry(PTR^.Left);
-      WRITE(PTR^.Key);
-      PrintSimmetry(PTR^.Right)
-    END
-END; 
+  PROCEDURE PrintSimmetry(PTR: NodeTree);
+  BEGIN
+    IF PTR <> NIL
+    THEN
+      BEGIN
+        PrintSimmetry(PTR^.Left);
+        WRITE(PTR^.Key);
+        PrintSimmetry(PTR^.Right)
+      END
+  END; 
 
-PROCEDURE TreePrintSimmetry();
-BEGIN
-  PrintSimmetry(Root)
-END; 
+  PROCEDURE TreePrintSimmetry();
+  BEGIN
+    PrintSimmetry(Root)
+  END; 
 
-PROCEDURE PrintStraight(PTR: NodeTree);
-BEGIN
-  IF PTR <> NIL
-  THEN
-    BEGIN
-      WRITE(PTR^.Key);
-      PrintStraight(PTR^.Left);
-      PrintStraight(PTR^.Right)
-    END
-END; 
+  PROCEDURE PrintStraight(PTR: NodeTree);
+  BEGIN
+    IF PTR <> NIL
+    THEN
+      BEGIN
+        WRITE(PTR^.Key);
+        PrintStraight(PTR^.Left);
+        PrintStraight(PTR^.Right)
+      END
+  END; 
 
-PROCEDURE TreePrintStraight();
-BEGIN
-  PrintStraight(Root)
-END; 
+  PROCEDURE TreePrintStraight();
+  BEGIN
+    PrintStraight(Root)
+  END; 
 
-PROCEDURE PrintReverse(PTR: NodeTree);
-BEGIN
-  IF PTR <> NIL
-  THEN
-    BEGIN
-      PrintReverse(PTR^.Left);
-      PrintReverse(PTR^.Right);
-      WRITE(PTR^.Key)
-    END
-END; 
+  PROCEDURE PrintReverse(PTR: NodeTree);
+  BEGIN
+    IF PTR <> NIL
+    THEN
+      BEGIN
+        PrintReverse(PTR^.Left);
+        PrintReverse(PTR^.Right);
+        WRITE(PTR^.Key)
+      END
+  END; 
 
-PROCEDURE TreePrintReverse();
-BEGIN
-  PrintReverse(Root)
-END; 
+  PROCEDURE TreePrintReverse();
+  BEGIN
+    PrintReverse(Root)
+  END; 
       
 BEGIN
   Root := NIL;
